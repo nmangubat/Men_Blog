297a7ddcf7b7aee21ca388e5ee138818a1e5adf7
nmangubat <aeon37@gmail.com>
2015-09-24 12:55:34 -0700

    add models, controllers

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,1 +1,6 @@
-routes.js
+var express = require('express');
+var apiRouter = express.Router();
+articlesController = require('../controllers/articles')
+var	Article = require('../controlllers/articles')
+
+apiRouter
